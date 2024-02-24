50e96e91acdbce3ec87892049fe02a93777f5a0c
haebyun <bhk4392@gmail.com>
2024-02-24 21:46:51 +0900

    만약, 멀티 쓰레드 환경이라면 HashMap보다는 ConcurrentHashMap을 쓰는 것이 안정적이며, sequence도 일반 long 자료형 대신 AtomicLong을 사용하는 것이 안정적이므로 변경해준다.

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -1,18 +1,19 @@
 package hello.itemservice.domain.item;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ItemRepository {
-    private static final Map<Long, Item> store = new HashMap<>();
-    private static long sequence = 0L;
+    private static final Map<Long, Item> store = new ConcurrentHashMap<>();
+    private static final AtomicLong sequence = new AtomicLong(0);
 
     public Item save(Item item) {
-        item.setId(++sequence);
+        item.setId(sequence.incrementAndGet());
         store.put(item.getId(), item);
         return item;
     }
