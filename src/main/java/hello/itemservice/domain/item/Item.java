4d90d4612b5ac90d5f948fc52fc16f5758fe5b63
haebyun <bhk4392@gmail.com>
2024-02-24 21:59:55 +0900

    Item 클래스에서 @Data를 쓰게되면 위험할 수 있으므로, @Getter와 @Setter만 사용하도록 한다.

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,8 +1,9 @@
 package hello.itemservice.domain.item;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter @Setter
 public class Item {
     private Long id;
     private String itemName;
